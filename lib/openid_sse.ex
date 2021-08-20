269546dc887d05871a50834c3aca68fcc5f570de
ritou <ritou.06@gmail.com>
2021-08-20 10:38:41 +0900

    initial commit

---
+++ lib/openid_sse.ex
@@ -0,0 +1,18 @@
+defmodule OpenidSse do
+  @moduledoc """
+  Documentation for `OpenidSse`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OpenidSse.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/openid_sse_test.exs
@@ -0,0 +1,8 @@
+defmodule OpenidSseTest do
+  use ExUnit.Case
+  doctest OpenidSse
+
+  test "greets the world" do
+    assert OpenidSse.hello() == :world
+  end
+end
